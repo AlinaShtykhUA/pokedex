83f2713fac267b1eaaedfcf2700fb2bc5ddc3b7c
Alina Shtykh <alinashtykh@gmail.com>
2023-08-11 18:26:24 +0200

    created capable-moves page

--- src/pages/pokemon-pages/CapableMoves.tsx
+++ src/pages/pokemon-pages/CapableMoves.tsx
@@ -1,7 +1,34 @@
 import React from 'react';
+import { useAppSelector } from '../../app/hooks';
 
 const CapableMoves = () => {
-  return <div>CapableMoves</div>;
+  const pokemonData = useAppSelector(
+    ({ pokemon: { currentPokemon } }) => currentPokemon
+  );
+
+  const pokemonAbility = pokemonData?.pokemonAbilities?.abilities?.map(
+    (ability: string) => (
+      <li key={ability} className="capable-moves__item ability">
+        {ability}
+      </li>
+    )
+  );
+  const pokemonMove = pokemonData?.pokemonAbilities?.moves?.map(
+    (move: string) => (
+      <li key={move} className="capable-moves__item">
+        {move}
+      </li>
+    )
+  );
+  return (
+    <div className="page capable-moves">
+      <h2 className="capable-moves__title">Abilities</h2>
+      <ul className="capable-moves__list">{pokemonAbility}</ul>
+
+      <h2 className="capable-moves__title">Moves</h2>
+      <ul className="capable-moves__list move">{pokemonMove}</ul>
+    </div>
+  );
 };
 
 export default CapableMoves;
